8185cead22e9dd5896350b394336adc149fa90e9
ShahRukh Ahmed <95552151+noircss@users.noreply.github.com>
2022-01-01 18:56:06 -0500

    Add files via upload

--- binarysearch.py
+++ binarysearch.py
@@ -1,6 +1,8 @@
 import random
 import time
+import sys
 
+sys.setrecursionlimit(10000)
 #naive search: scan entire list and ask if its equal to the target
 # if yes, return the index
 # if no, then return -1
@@ -15,15 +17,16 @@ def naive_search(l, target):
 #binary search uses divide and conquer
 #we will leverage the fact that our list is SORTED
 def binary_search(l, target, low=None, high=None):
+    #example l = [1, 3, 5, 10, 12] #should return 3
     if low is None:
         low = 0
     if high is None:
-        high = len(l) -1
+        high = len(l) - 1
 
     if high < low:
         return -1
-    #example l = [1, 3, 5, 10, 12] #should return 3
-    midpoint = (len(l)) // 2 # 2
+
+    midpoint = (low + high) // 2 # 2
 
     if l[midpoint] == target:
         return midpoint
@@ -34,10 +37,10 @@ def binary_search(l, target, low=None, high=None):
         return binary_search(l, target, midpoint+1, high)
 
 if __name__=='__main__':
-    l = [1, 3, 5, 10, 12]
-    target = 10
-    print(naive_search(l, target))
-    print(binary_search(l, target))
+    #l = [1, 3, 5, 10, 12]
+    #target = 10
+    #print(naive_search(l, target))
+    #print(binary_search(l, target))
 
 
     length = 10000
